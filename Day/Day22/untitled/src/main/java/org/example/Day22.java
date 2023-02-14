1f929470e9d8129e12fcfa57f074858ddc6e7908
Hyeonseo-Lee <87067373+Hyeonseo-Lee@users.noreply.github.com>
2023-02-14 10:24:03 +0900

    01

---
+++ Day/Day22/untitled/src/main/java/org/example/Day22.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+
+import static java.lang.Thread.sleep;
+
+public class Day22 {
+    public static void main(String args[]) {
+        System.out.println("멀티캐스트 타임 서버");
+        DatagramSocket serverSocket = null; // UDP
+        try {
+            serverSocket = new DatagramSocket();
+            while (true) {
+                String dateText = new Date().toString();
+                byte[] buffer = new byte[256];
+                buffer = dateText.getBytes();
+                InetAddress group = InetAddress.getByName("224.0.0.7");
+                DatagramPacket packet;
+                packet = new DatagramPacket(buffer, buffer.length, group, 10000);
+                serverSocket.send(packet);
+                System.out.println("전송된 시각: " + dateText);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    // Handle exception
+                }
+            }
+
+        } catch (IOException ex) {
+            // Handle exception
+        }
+        }
+    }
